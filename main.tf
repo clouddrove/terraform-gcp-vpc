d3bac9da91874fbe6f339b545443ceec5501f71c
theprashantyadav <theprashantyadav7@gmail.com>
2022-11-18 18:12:24 +0530

    inital commit

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "labels" {
-  source       = "git::https://github.com/clouddrove/terraform-gcp-labels.git?ref"
+  source = "git::https://github.com/clouddrove/terraform-gcp-labels.git?ref"
 
   name        = var.name
   environment = var.environment

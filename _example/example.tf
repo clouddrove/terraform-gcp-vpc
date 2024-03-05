d65c429377442f0003f4e649a39069b5fae9a8d9
Kkhandale2024 <kalyani.khandale@clouddrove.com>
2024-03-06 00:10:59 +0530

    feat: added shared vpc module

--- _example/example.tf
+++ _example/example.tf
@@ -12,12 +12,14 @@ module "vpc" {
   environment = var.environment
   label_order = var.label_order
 
-  google_compute_network_enabled  = true
-  project_id                      = "clouddrove"
-  auto_create_subnetworks         = false
-  routing_mode                    = "REGIONAL"
-  mtu                             = 1460
-  delete_default_routes_on_create = true
-  enable_ula_internal_ipv6        = false
-
+  google_compute_network_enabled         = true
+  project_id                             = "dev-env-3b53"
+  auto_create_subnetworks                = false
+  routing_mode                           = "REGIONAL"
+  mtu                                    = 1460
+  delete_default_routes_on_create        = true
+  enable_ula_internal_ipv6               = false
+  google_compute_shared_vpc_host_enabled = true
+  host_project_id                        = "dev-env-3b53"
+  service_project_id                     = "bamboo-analyst-416214"
 }
